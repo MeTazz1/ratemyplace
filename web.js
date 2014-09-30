56f22561bcfc5bbf5cba7585869bd52466e4b921
christophe <christophedellac@gmail.com>
2014-09-30 22:44:23 +0200

    test cool

--- web.js
+++ web.js
@@ -1,11 +1,12 @@
 var express = require("express");
 var logfmt = require("logfmt");
 var app = express();
+var cool = require('cool-ascii-faces');
 
 app.use(logfmt.requestLogger());
 
 app.get('/', function(req, res) {
-	res.send('Hello World!');
+	res.send(cool());
     });
 
 var port = Number(process.env.PORT || 5000);
