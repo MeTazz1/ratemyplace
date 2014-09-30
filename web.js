6041c4a55527460b154c995f5aad3520c786601d
christophe <christophedellac@gmail.com>
2014-09-30 23:22:32 +0200

    semver

--- web.js
+++ web.js
@@ -1,3 +1,20 @@
+var express = require("express");
+var logfmt = require("logfmt");
+var app = express();
+var cool = require('cool-ascii-faces');
+
+app.use(logfmt.requestLogger());
+
+app.get('/', function(req, res) {
+	res.send(cool());
+    });
+
+var port = Number(process.env.PORT || 5000);
+app.listen(port, function() {
+	console.log("Listening on " + port);
+    });
+
+
 var util = require('util'),
     http = require('http'),
     fs = require('fs'),
